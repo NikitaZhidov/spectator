1a90bb4c5133490b78294df5dc221e297e9a27be
NikitaZhidov <nzhidov02@gmail.com>
2021-11-12 00:37:26 +0300

    #3: Remove unnecessary code in MainPage

--- apps/spectator/src/App/pages/MainPage.tsx
+++ apps/spectator/src/App/pages/MainPage.tsx
@@ -1,10 +1,5 @@
-import axios from 'axios';
-import React, { useEffect } from 'react';
+import React from 'react';
 
 export const MainPage: React.FC = () => {
-	useEffect(() => {
-		axios.get('/api/auth/test').then((res) => console.log(res.data));
-	});
-
 	return <div>MainPage</div>;
 };
